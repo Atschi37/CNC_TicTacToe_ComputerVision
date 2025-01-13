b900770b93782bff10e1d86dfbddff1eeac35f32
atschi37 <aaron.koeditz@mrge.lernsax.de>
2025-01-13 14:48:23 +0100

    Füge Funktionen zum Zeichnen von Gewinnlinien und Zügen hinzu, verbessere G-Code-Streaming für Tic Tac Toe

--- Vision+Analyse+Plotting.py
+++ Vision+Analyse+Plotting.py
@@ -22,6 +22,46 @@ print("Starte Video Analyse...")
 threading.Thread(target=start_game).start()
 print("Video Analyse gestartet.")
 
+def draw_win(win_type, ser):
+    if win_type == "Row 0":
+        Gcode_Streamer.stream_gcode(ser, "gcode/wh0.ngc")
+    elif win_type == "Row 1":
+        Gcode_Streamer.stream_gcode(ser, "gcode/wh1.ngc")
+    elif win_type == "Row 2":
+        Gcode_Streamer.stream_gcode(ser, "gcode/wh2.ngc")
+    elif win_type == "Column 0":
+        Gcode_Streamer.stream_gcode(ser, "gcode/wv0.ngc")
+    elif win_type == "Column 1":
+        Gcode_Streamer.stream_gcode(ser, "gcode/wv1.ngc")
+    elif win_type == "Column 2":
+        Gcode_Streamer.stream_gcode(ser, "gcode/wv2.ngc")
+    elif win_type == "Main diagonal":
+        Gcode_Streamer.stream_gcode(ser, "gcode/wdl-r.ngc")
+    elif win_type == "Anti-diagonal":
+        Gcode_Streamer.stream_gcode(ser, "gcode/wdr-l.ngc")
+    Gcode_Streamer.raise_toolhead(ser, 15)
+
+def draw_move(move, ser):
+    if move == (0, 0):
+        Gcode_Streamer.stream_gcode(ser, "gcode/dreieck_0-0.ngc")
+    elif move == (0, 1):
+        Gcode_Streamer.stream_gcode(ser, "gcode/dreieck_0-1.ngc")
+    elif move == (0, 2):
+        Gcode_Streamer.stream_gcode(ser, "gcode/dreieck_0-2.ngc")
+    elif move == (1, 0):
+        Gcode_Streamer.stream_gcode(ser, "gcode/dreieck_1-0.ngc")
+    elif move == (1, 1):
+        Gcode_Streamer.stream_gcode(ser, "gcode/dreieck_1-1.ngc")
+    elif move == (1, 2):
+        Gcode_Streamer.stream_gcode(ser, "gcode/dreieck_1-2.ngc")
+    elif move == (2, 0):
+        Gcode_Streamer.stream_gcode(ser, "gcode/dreieck_2-0.ngc")
+    elif move == (2, 1):
+        Gcode_Streamer.stream_gcode(ser, "gcode/dreieck_2-1.ngc")
+    elif move == (2, 2):
+        Gcode_Streamer.stream_gcode(ser, "gcode/dreieck_2-2.ngc")
+    Gcode_Streamer.stream_gcode(ser, "gcode/scanpos.ngc")
+
 while True:
     command = input("[1] Ursprung setzen und Kopf heben (G92)\n [2] G-Code streamen\n [3] Werkzeugkopf anheben\n [4] Besten Zug für T berechenen\n [5] Move to Scan Position\n [6] Automatisch besten Zug ausführen\n [7] Beenden\n")
     if command == "1":
@@ -46,8 +86,26 @@ while True:
                 best_move = TicTacToeAI.find_best_move(current_board)
                 print(f"Best move for 'T' is: {best_move}")
     elif command == "5":
-        Gcode_Streamer.stream_gcode(ser, "scanpos.ngc")
+        Gcode_Streamer.stream_gcode(ser, "gcode/scanpos.ngc")
     elif command =="6":
-        pass
+
+        urrent_board = game.get_current_board()
+        print(current_board)
+        winner, win_type = TicTacToeAI.is_winner(current_board, 'T')
+        if winner:#Check if 'T' wins and draw Line to mark win
+            print(f"'T' wins! {win_type}")
+            draw_win(win_type, ser)
+        else:
+            winner, win_type = TicTacToeAI.is_winner(current_board, 'O')
+            if winner:# Check if 'O' wins and draw Line to mark win
+                print(f"'O' wins! {win_type}")
+                draw_win(win_type, ser)
+            else: #Find and draw best move
+
+                best_move = TicTacToeAI.find_best_move(current_board)
+                print(f"Best move for 'T' is: {best_move}")
+                draw_move(best_move, ser)
+
+
     elif command == "7":
         break
